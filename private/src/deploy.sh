1b34a1c370d89d916d529bcc92888d5f6ac57fc0
Noah-Giustini <noah.giu1@gmail.com>
2026-01-18 13:32:28 -0700

    fix bug in deploy script

--- private/src/deploy.sh
+++ private/src/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #pull latest changes from master
-cd /home/giraffe/repo/win98_personal_site
+cd /home/giraffe/repos/win98_personal_site
 git pull
 
 #move the site files to the deployment directory
 sudo rm -rf /var/www/html/*
-sudo cp -r /home/giraffe/repo/win98_personal_site/public/* /var/www/html/
+sudo cp -r /home/giraffe/repos/win98_personal_site/public/* /var/www/html/
 
 #restart the API service to pick up any changes
 sudo systemctl restart giraffe-net-api.service
