ce1000433d6c682aabcbd6d9ffb5a5f319be588c
Noah-Giustini <noah.giu1@gmail.com>
2026-01-25 13:09:15 -0700

    modified api to have different gpu/temp logic for non-jetson vs jetson devices

--- private/src/api.py
+++ private/src/api.py
@@ -13,8 +13,31 @@ import time
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+def run_command(command: str):
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        return result.stdout if result.returncode == 0 else result.stderr
+    except Exception as e:
+        return str(e)
+    
 # Load variables from .env
 load_dotenv()
+
+# Pull configuration from environment variables
+API_KEY = os.getenv("API_KEY")
+API_KEY_NAME = os.getenv("API_KEY_NAME", "access_token")
+SERVER_IP = os.getenv("SERVER_IP", "0.0.0.0")
+SERVER_PORT = int(os.getenv("SERVER_PORT", 8000))
+MC_SERVER_DIR = os.getenv("MC_SERVER_DIR", "/opt/minecraft/server")
+
+jetson_device = False
+# Check if running on NVIDIA Jetson device
+if (run_command("which tegrastats").strip() != ""):
+    jetson_device = True
+    print("Jetson device detected. Enabling GPU and temperature metrics.")
+else:
+    jetson_device = False
+    print("No Jetson device detected. GPU and temperature metrics will be simulated.")
 
 app = FastAPI(title="Giraffe-Net Server API")
 
@@ -26,13 +49,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Pull configuration from environment variables
-API_KEY = os.getenv("API_KEY")
-API_KEY_NAME = os.getenv("API_KEY_NAME", "access_token")
-SERVER_IP = os.getenv("SERVER_IP", "0.0.0.0")
-SERVER_PORT = int(os.getenv("SERVER_PORT", 8000))
-MC_SERVER_DIR = os.getenv("MC_SERVER_DIR", "/opt/minecraft/server")
-
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
 async def get_api_key(header_key: str = Depends(api_key_header)):
@@ -42,13 +58,6 @@ async def get_api_key(header_key: str = Depends(api_key_header)):
         status_code=HTTP_403_FORBIDDEN, 
         detail="Invalid API Key"
     )
-
-def run_command(command: str):
-    try:
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return result.stdout if result.returncode == 0 else result.stderr
-    except Exception as e:
-        return str(e)
     
 def get_system_metrics():
     """Collects real-time system usage metrics."""
@@ -63,22 +72,28 @@ def get_system_metrics():
     mem_total_gb = round(memory.total / (1024 ** 3), 1)
     mem_percent = memory.percent
 
-    # 3. GPU Usage and Temperature (specific to NVIDIA Jetson devices)
-    #this info is pulled through tegrastats. we parse the output to get gpu usage and cpu temp but there is more info available if needed.
-    #we set interval 100 to get a fast response. head is used to only get the first line and then kill the process.
-    gpu_temp_cmd = r"""tegrastats --interval 100 | head -n 1 | awk '{for(i=1;i<=NF;i++){if($i=="GR3D_FREQ") g=$(i+1); if($i~"cpu@") {split($i,a,"@"); c=a[2]}} print g","c}'"""
-    gpu_temp_string = run_command(gpu_temp_cmd)
-    gpu_temp_string = gpu_temp_string.strip().split(",")
-    gpu_percent = float(gpu_temp_string[0].strip("%"))
-    cpu_temp = float(gpu_temp_string[1].strip("C"))
+    gpu_percent = 0.0
+    cpu_temp = 0.0
 
-    #the below items are placeholders incase the API is not running on a jetson device.
-    # For simulation purposes, we will generate a fake temperature based on CPU usage
-    # base_temp = 30 
-    # cpu_temp = round(base_temp + (cpu_percent * 0.15))
+    if (jetson_device == True):
+        # 3. GPU Usage and Temperature (specific to NVIDIA Jetson devices)
+        #this info is pulled through tegrastats. we parse the output to get gpu usage and cpu temp but there is more info available if needed.
+        #we set interval 100 to get a fast response. head is used to only get the first line and then kill the process.
+        gpu_temp_cmd = r"""tegrastats --interval 100 | head -n 1 | awk '{for(i=1;i<=NF;i++){if($i=="GR3D_FREQ") g=$(i+1); if($i~"cpu@") {split($i,a,"@"); c=a[2]}} print g","c}'"""
+        gpu_temp_string = run_command(gpu_temp_cmd)
+        gpu_temp_string = gpu_temp_string.strip().split(",")
+        gpu_percent = float(gpu_temp_string[0].strip("%"))
+        cpu_temp = float(gpu_temp_string[1].strip("C"))
+    else:
+         #the below items are placeholders incase the API is not running on a jetson device.
+        # For simulation purposes, we will generate a fake temperature based on CPU usage
+        base_temp = 30 
+        cpu_temp = round(base_temp + (cpu_percent * 0.15))
 
-    # Simulated GPU usage based on CPU usage
-    # gpu_percent = round((cpu_percent * 0.5) % 100) # Simple formula for simulation
+        # Simulated GPU usage based on CPU usage
+        gpu_percent = round((cpu_percent * 0.5) % 100) # Simple formula for simulation
+
+   
 
     return {
         'cpu_percent': cpu_percent,
